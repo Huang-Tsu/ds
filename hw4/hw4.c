e47c0c61df1dd30c22415a86cffd5c51d04c6a8d
Huang-Tsu <huang0966714@gmail.com>
2022-01-01 22:14:53 +0800

    try bit field

--- hw4/hw4.c
+++ hw4/hw4.c
@@ -1,10 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct BitField BitField;
+
+struct BitField{
+	unsigned char test1:1;
+	unsigned char test2:1;
+	unsigned char test3:1;
+	unsigned char test4:1;
+	unsigned char test5:1;
+	unsigned char test6:1;
+	unsigned char test7:1;
+	unsigned char test8:1;
+}*g_test;
+
 int main(){
 	int n, m, p;
 	
-	scanf("%d%d%d", &p, &m, &p);
+	scanf("%d%d%d", &n, &m, &p);
+
+	g_test = (BitField*)calloc(n, sizeof(BitField));
+	
 
 	return 0;
 }
