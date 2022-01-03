91d00e026556ad7c5db5998978e63fab47c7f16d
Huang-Tsu <huang0966714@gmail.com>
2022-01-03 15:52:01 +0800

    minor change: from /8 to >>3

--- hw4/hw4.c
+++ hw4/hw4.c
@@ -17,138 +17,130 @@ struct BitField{
 	unsigned char bit7:1;
 };
 
-int g_m, g_p;
 
 //int Hash(int input);
 //int SetBitField(int offset, int bit_number);
 
 int main(){
-	int n;
+	int n, m, p;
 	int input;
 	
-	scanf("%d%d%d", &g_p, &g_m, &n);
+	scanf("%d%d%d", &p, &m, &n);
 
-	//g_test = (BitField*)calloc(g_m/8+1, sizeof(BitField));	//one bit_fielt is 8 bits, so we need m/8 + 1 bit_field
-	BitField g_test[g_m/8+1];
+	//g_test = (BitField*)calloc(m>>3+1, sizeof(BitField));	//one bit_fielt is 8 bits, so we need m>>3 + 1 bit_field
+	BitField g_test[(m>>3)+1];
 
-		for(int i=0; i<g_m; i++){
+		for(int i=0; i<m; i++){
 			switch(i%8){
 				case 0:
-					g_test[i/8].bit0 = 0;
+					g_test[i>>3].bit0 = 0;
 					break;
 
 				case 1:
-					g_test[i/8].bit1 = 0;
+					g_test[i>>3].bit1 = 0;
 					break;
 
 				case 2:
-					g_test[i/8].bit2 = 0;
+					g_test[i>>3].bit2 = 0;
 					break;
 
 				case 3:
-					g_test[i/8].bit3 = 0;
+					g_test[i>>3].bit3 = 0;
 					break;
 
 				case 4:
-					g_test[i/8].bit4 = 0;
+					g_test[i>>3].bit4 = 0;
 					break;
 
 				case 5:
-					g_test[i/8].bit5 = 0;
+					g_test[i>>3].bit5 = 0;
 					break;
 
 				case 6:
-					g_test[i/8].bit6 = 0;
+					g_test[i>>3].bit6 = 0;
 					break;
 
 				case 7:
-					g_test[i/8].bit7 = 0;
+					g_test[i>>3].bit7 = 0;
 					break;
 			}
 		}
+			//get data and process it
 	for(int i=0; i<n; i++){
 		scanf("%d", &input);
 		printf("%d ", input);
-
 		//input = Hash(input);
-			input = ((input*input)%g_p)%g_m;
+			input = ((input*input)%p)%m;
 			switch(input%8){
 				case 0:
-					if(g_test[input/8].bit0 == 0){		//this place haven't been used
-						g_test[input/8].bit0 = 1;
+					if(g_test[input>>3].bit0 == 0){		//this place haven't been used
+						g_test[input>>3].bit0 = 1;
 						puts("1");
+						continue;
 					}
-					else
-						puts("0");
 					break;
 
 				case 1:
-					if(g_test[input/8].bit1 == 0){		//this place haven't been used
-						g_test[input/8].bit1 = 1;
+					if(g_test[input>>3].bit1 == 0){		//this place haven't been used
+						g_test[input>>3].bit1 = 1;
 						puts("1");
+						continue;
 					}
-					else
-						puts("0");
 					break;
 
 				case 2:
-					if(g_test[input/8].bit2 == 0){		//this place haven't been used
-						g_test[input/8].bit2 = 1;
+					if(g_test[input>>3].bit2 == 0){		//this place haven't been used
+						g_test[input>>3].bit2 = 1;
 						puts("1");
+						continue;
 					}
-					else
-						puts("0");
 					break;
 
 				case 3:
-					if(g_test[input/8].bit3 == 0){		//this place haven't been used
-						g_test[input/8].bit3 = 1;
+					if(g_test[input>>3].bit3 == 0){		//this place haven't been used
+						g_test[input>>3].bit3 = 1;
 						puts("1");
+						continue;
 					}
-					else
-						puts("0");
 					break;
 					
 				case 4:
-					if(g_test[input/8].bit4 == 0){		//this place haven't been used
-						g_test[input/8].bit4 = 1;
+					if(g_test[input>>3].bit4 == 0){		//this place haven't been used
+						g_test[input>>3].bit4 = 1;
 						puts("1");
+						continue;
 					}
-					else
-						puts("0");
 					break;
 
 				case 5:
-					if(g_test[input/8].bit5 == 0){		//this place haven't been used
-						g_test[input/8].bit5 = 1;
+					if(g_test[input>>3].bit5 == 0){		//this place haven't been used
+						g_test[input>>3].bit5 = 1;
 						puts("1");
+						continue;
 					}
-					else
-						puts("0");
 					break;
 
 				case 6:
-					if(g_test[input/8].bit6 == 0){		//this place haven't been used
-						g_test[input/8].bit6 = 1;
+					if(g_test[input>>3].bit6 == 0){		//this place haven't been used
+						g_test[input>>3].bit6 = 1;
 						puts("1");
+						continue;
 					}
-					else
-						puts("0");
 					break;
 
 				case 7:
-					if(g_test[input/8].bit7 == 0){		//this place haven't been used
-						g_test[input/8].bit7 = 1;
+					if(g_test[input>>3].bit7 == 0){		//this place haven't been used
+						g_test[input>>3].bit7 = 1;
 						puts("1");
+						continue;
 					}
-					else
-						puts("0");
 					break;
 			}
+			puts("0");	//bit field have been used
 	}
 
 			/*
-		if(SetBitField(input/8, input%8))
+		if(SetBitField(input>>3, input%8))
 			puts("1");
 		else
 			puts("0");
