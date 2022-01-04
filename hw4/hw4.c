1c9fccedcb482381ab884c3d2221947565d04031
Huang-Tsu <huang0966714@gmail.com>
2022-01-04 14:34:04 +0800

    some change in hw4.c

--- hw4/hw4.c
+++ hw4/hw4.c
@@ -15,7 +15,7 @@ struct BitField{
 	unsigned char bit5:1;
 	unsigned char bit6:1;
 	unsigned char bit7:1;
-};
+}*g_test;
 
 
 //int Hash(int input);
@@ -27,51 +27,56 @@ int main(){
 	
 	scanf("%d%d%d", &p, &m, &n);
 
-	//g_test = (BitField*)calloc(m>>3+1, sizeof(BitField));	//one bit_fielt is 8 bits, so we need m>>3 + 1 bit_field
-	BitField g_test[(m>>3)+1];
+	g_test = (BitField*)calloc((m>>3)+1, sizeof(BitField));	//one bit_fielt is 8 bits, so we need m>>3 + 1 bit_field
+	//BitField g_test[(m>>3)+1];
 
-		for(int i=0; i<m; i++){
-			switch(i%8){
-				case 0:
-					g_test[i>>3].bit0 = 0;
-					break;
+	/*
+		//initialize
+	for(int i=0; i<m; i++){
+		switch(i & ((1<<3)-1)){	//1<<3 = 8, i & ((1<<3)-1)即為取小於(1<<3)的位元為1的部分
+			case 0:
+				g_test[i>>3].bit0 = 0;
+				break;
 
-				case 1:
-					g_test[i>>3].bit1 = 0;
-					break;
+			case 1:
+				g_test[i>>3].bit1 = 0;
+				break;
 
-				case 2:
-					g_test[i>>3].bit2 = 0;
-					break;
+			case 2:
+				g_test[i>>3].bit2 = 0;
+				break;
 
-				case 3:
-					g_test[i>>3].bit3 = 0;
-					break;
+			case 3:
+				g_test[i>>3].bit3 = 0;
+				break;
 
-				case 4:
-					g_test[i>>3].bit4 = 0;
-					break;
+			case 4:
+				g_test[i>>3].bit4 = 0;
+				break;
 
-				case 5:
-					g_test[i>>3].bit5 = 0;
-					break;
+			case 5:
+				g_test[i>>3].bit5 = 0;
+				break;
 
-				case 6:
-					g_test[i>>3].bit6 = 0;
-					break;
+			case 6:
+				g_test[i>>3].bit6 = 0;
+				break;
 
-				case 7:
-					g_test[i>>3].bit7 = 0;
-					break;
-			}
+			case 7:
+				g_test[i>>3].bit7 = 0;
+				break;
 		}
+	}
+	*/
 			//get data and process it
 	for(int i=0; i<n; i++){
 		scanf("%d", &input);
 		printf("%d ", input);
 		//input = Hash(input);
+
 			input = ((input*input)%p)%m;
-			switch(input%8){
+
+			switch(input & ((1<<3)-1)){		//0b1000 = 8	-> input%8
 				case 0:
 					if(g_test[input>>3].bit0 == 0){		//this place haven't been used
 						g_test[input>>3].bit0 = 1;
